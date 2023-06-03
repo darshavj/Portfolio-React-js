f03e9aa2dce09eab18ab2ed295aaf6e9e5ca893e
darshavj <darshavj094@gmail.com>
2023-06-03 11:44:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-// import Temp from './components/Temp'
 
 const App = () => {
   return (
